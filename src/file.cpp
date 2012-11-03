fd45ed11e59af4ed4f461e26ce75a5e3f22037d8
Christopher Hinz <christopher_m.hinz@t-online.de>
2012-11-03 16:06:49 +0100

    cosmetic changes

--- src/file.cpp
+++ src/file.cpp
@@ -3,8 +3,7 @@
 namespace echelon
 {
 file::file(const std::string& path, create_mode mode)
-:
-  file_wrapper_(path,
+: file_wrapper_(path,
                 mode == create_mode::truncate ? H5F_ACC_TRUNC : H5F_ACC_EXCL,
                 H5P_DEFAULT,
                 H5P_DEFAULT),
@@ -13,8 +12,7 @@ file::file(const std::string& path, create_mode mode)
 }
 
 file::file(const std::string& path, open_mode mode)
-:
-  file_wrapper_(path,
+: file_wrapper_(path,
                 mode == open_mode::read_only ? H5F_ACC_RDONLY : H5F_ACC_RDWR,
                 H5P_DEFAULT),
   root_group_(*this)
