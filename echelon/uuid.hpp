bae9ad662a3b810a272a87e51e5b5cfae4ee3116
Christopher Hinz <christopher_m.hinz@t-online.de>
2013-03-01 22:17:17 +0100

    silences a unused variable warning

--- echelon/uuid.hpp
+++ echelon/uuid.hpp
@@ -6,7 +6,7 @@
 namespace echelon
 {
 
-static const char* uuid = "{9160ef20-47c3-11e2-bcfd-0800200c9a66}";
+constexpr char uuid[] = "{9160ef20-47c3-11e2-bcfd-0800200c9a66}";
 
 std::string generate_unique_identifier(const std::string& name);
 
