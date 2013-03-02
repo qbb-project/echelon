b67fde09537cfa3ba54d65633264c1005b5c1e8e
Christopher Hinz <christopher_m.hinz@t-online.de>
2013-03-02 14:22:11 +0100

    adds missing copyright notices

--- echelon/uuid.hpp
+++ echelon/uuid.hpp
@@ -1,3 +1,8 @@
+//  Copyright (c) 2013 Christopher Hinz
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef ECHELON_UUID_HPP
 #define ECHELON_UUID_HPP
 
