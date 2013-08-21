5033fd71ac3800c55ca180b3f52ecda9709e6fca
Christopher Hinz <christopher_m.hinz@t-online.de>
2013-08-21 20:50:25 +0200

    fixes the build system for cmake 2.8.12

--- cmake/generate_imported_library_targets.cmake
+++ cmake/generate_imported_library_targets.cmake
@@ -1,4 +1,4 @@
-#  Copyright (c) 2012 Christopher Hinz
+#  Copyright (c) 2012-2013 Christopher Hinz
 #
 #  Distributed under the Boost Software License, Version 1.0. (See accompanying
 #  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -10,23 +10,23 @@ get_filename_component(target_ext ${lib_path} EXT)
 
 if(NOT TARGET imported_target_${target_name})
 
-if(${target_ext} STREQUAL ".so")
+if(${target_ext} MATCHES ".so")
 
   add_library(imported_target_${target_name} SHARED IMPORTED)
 
-else(${target_ext} STREQUAL ".so")
+else(${target_ext} MATCHES ".so")
 
- if(${target_ext} STREQUAL ".a")
+ if(${target_ext} MATCHES ".a")
    
   add_library(imported_target_${target_name} STATIC IMPORTED)
 
- else(${target_ext} STREQUAL ".a")
+ else(${target_ext} MATCHES ".a")
 
    message(SEND_ERROR "${target_ext} is not a valid library extension")
 
- endif(${target_ext} STREQUAL ".a")
+ endif(${target_ext} MATCHES ".a")
 
-endif(${target_ext} STREQUAL ".so")
+endif(${target_ext} MATCHES ".so")
 
 set_property(TARGET imported_target_${target_name} PROPERTY IMPORTED_LOCATION ${lib_path})
 
@@ -63,22 +63,40 @@ function(generate_imported_library_targets library_paths imported_target_list)
 
 foreach(lib_path ${${library_paths}})
 
-if((${lib_path} STREQUAL "debug") OR (${lib_path} STREQUAL "optimized") OR (${lib_path} STREQUAL "general")
-   OR (${lib_path} STREQUAL "LINK_PRIVATE") OR (${lib_path} STREQUAL "LINK_PUBLIC"))
+if(lib_path)
 
-  list(APPEND imported_target_list_proxy ${lib_path})
+    if((${lib_path} STREQUAL "debug") OR (${lib_path} STREQUAL "optimized") OR (${lib_path} STREQUAL "general")
+    OR (${lib_path} STREQUAL "LINK_PRIVATE") OR (${lib_path} STREQUAL "LINK_PUBLIC"))
 
-elseif(${lib_path} MATCHES "^-[^ \\f\\n\\r\\t\\v]+$")
+        set(previous_library_type_specifier ${lib_path})
 
-  list(APPEND imported_target_list_proxy ${lib_path})
+    elseif(${lib_path} MATCHES "^-[^ \\f\\n\\r\\t\\v]+$")
 
-else()
+        if(previous_library_type_specifier)
 
-  add_imported_target(${lib_path})
+            list(APPEND imported_target_list_proxy ${previous_library_type_specifier})
+            set(previous_library_type_specifier NO)
+
+        endif() 
+
+        list(APPEND imported_target_list_proxy ${lib_path})
+
+    else()
+
+        if(previous_library_type_specifier)
+
+            list(APPEND imported_target_list_proxy ${previous_library_type_specifier})
+            set(previous_library_type_specifier NO)         
+
+        endif()
+
+        add_imported_target(${lib_path})
+
+    endif()
 
 endif()
 
-endforeach(lib_path)
+endforeach()
 
 set(${imported_target_list} ${imported_target_list_proxy} PARENT_SCOPE)
 
