a0f3021357ee72e1dd3ae371258802ea7f19574e
Christopher Hinz <hinz@theo-physik.uni-kiel.de>
2014-11-14 14:27:44 +0100

    revert the format version back to 1.0.0 for now

--- src/file.cpp
+++ src/file.cpp
@@ -15,7 +15,7 @@ file::file(const std::string& path, create_mode mode)
   root_group_(*this)
 {
     root_group_.attributes().create("echelon.library_version", "0.7.0");
-    root_group_.attributes().create("echelon.format_version", "0.7.0");
+    root_group_.attributes().create("echelon.format_version", "1.0.0");
 }
 
 file::file(const std::string& path, open_mode mode)
