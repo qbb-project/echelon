1394aa609293f3da488b1653c10f6581ae403cbf
Christopher Hinz <hinz@theo-physik.uni-kiel.de>
2015-05-12 20:54:14 +0200

    Add a test case for the group require API.

--- tests/require_api.cpp
+++ tests/require_api.cpp
@@ -12,13 +12,36 @@
 
 #include "basic_fixture.hpp"
 
-BOOST_FIXTURE_TEST_CASE( require_api, basic_fixture )
-{   
-      echelon::group g1 = temp_file.require_group("g1");
-    
-      BOOST_CHECK_NO_THROW(temp_file["g1"]);
-      
-      echelon::dataset ds1 = temp_file.create_dataset<double>("ds1",{ 10 , 10 });
-      
-      BOOST_CHECK_THROW(ds1 = temp_file.require_dataset<double>("ds1",{ 20 , 10 }),echelon::hdf5::broken_contract_exception);
+BOOST_AUTO_TEST_SUITE(require_api_suite)
+
+BOOST_FIXTURE_TEST_CASE(require_api, basic_fixture)
+{
+    echelon::group g1 = temp_file.require_group("g1");
+
+    BOOST_CHECK_NO_THROW(temp_file["g1"]);
+
+    echelon::dataset ds1 = temp_file.create_dataset<double>("ds1", {10, 10});
+
+    BOOST_CHECK_NO_THROW(temp_file["ds1"]);
+
+    BOOST_CHECK_THROW(ds1 = temp_file.require_dataset<double>("ds1", {20, 10}),
+                      echelon::hdf5::broken_contract_exception);
 }
+
+BOOST_FIXTURE_TEST_CASE(group_require_api, basic_fixture)
+{
+    echelon::group g = temp_file.create_group("g");
+
+    g.require_group("g1");
+
+    BOOST_CHECK_NO_THROW(g["g1"]);
+
+    g.require_dataset<double>("ds1", {10, 10});
+
+    BOOST_CHECK_NO_THROW(g["ds1"]);
+
+    BOOST_CHECK_THROW(g.require_dataset<double>("ds1", {20, 10}),
+        echelon::hdf5::broken_contract_exception);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
