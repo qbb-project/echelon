68d179c688243064a380d195ffad66f036b08e19
Christopher Hinz <hinz@theo-physik.uni-kiel.de>
2015-05-22 13:44:32 +0200

    Add a missing copyright header.
    [skip CI]

--- echelon/hdf5/array_slice.hpp
+++ echelon/hdf5/array_slice.hpp
@@ -1,3 +1,8 @@
+//  Copyright (c) 2015 Christopher Hinz
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef ECHELON_HDF5_ARRAY_SLICE_HPP
 #define ECHELON_HDF5_ARRAY_SLICE_HPP
 
